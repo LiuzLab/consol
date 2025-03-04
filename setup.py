d245b1766515b1a8a9f7ef95bae396d433042e4b
jaeyeon <Jaeyeon.Lee@bcm.edu>
2025-03-04 11:06:09 -0600

    List required packages

--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version="0.1.0",
     packages=find_packages('bin'),
     package_dir={'': 'bin'},
-    install_requires=[],
+    install_requires=[
+        'dotenv>=0.9.9',
+        'scipy>=1.15.2',
+        'langchain>=0.3.14',
+        'langchain-openai>=0.3.3',
+        'pandas>=2.2.3',
+    ],
     entry_points={
         'console_scripts': [
             'consol-cli=consol.main:main',
@@ -17,5 +23,5 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/LiuzLab/consol",
-    python_requires=">=3.6",
+    python_requires=">=3.11.0",
 )
